3f6480d1c726a202f406dd19ce5e064a8b91ce32
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-10-24 20:37:35 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'ec2-50-19-95-77.compute-1.amazonaws.com',
+  DB_URL: process.env.DB_URL || 'postgresql://localhost',
 };
